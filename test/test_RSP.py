5d6d1b4dc8647c30071697e82017fafeb6d926c3
Adriceka <adrigonzalezgonzalez2007@gmail.com>
2026-02-12 09:55:45 +0100

    test: Añadir casos test para piedra
    Añadir casos test para cuando se elige la piedra

--- test/test_RSP.py
+++ test/test_RSP.py
@@ -23,3 +23,22 @@ def test_draw(game):
         user_action=GameAction.SCISSORS,
         computer_action=GameAction.SCISSORS)
 
+
+# =========================
+# ROCK/PIEDRA GANA O PIERDE
+# =========================
+
+@pytest.mark.rock
+def test_rock_wins(game):
+    assert GameResult.VICTORY == game.assess_game(
+        user_action=GameAction.ROCK,
+        computer_action=GameAction.SCISSORS)
+
+
+@pytest.mark.rock
+def test_rock_loses(game):
+    assert GameResult.DEFEAT == game.assess_game(
+        user_action=GameAction.ROCK,
+        computer_action=GameAction.PAPER)
+
+
