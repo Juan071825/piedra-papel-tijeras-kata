d97e8b57644f45e9a559b0e31f06a60e514bf90d
Juan071825 <juanmateoalvarez@gmail.com>
2026-02-13 14:07:11 +0100

    feat(RPS_dict): :sparkles: Aplicar programación orientada a objetos.

--- src/RPS_dict.py
+++ src/RPS_dict.py
@@ -15,21 +15,21 @@ class GameResult(IntEnum):
     Tie = 2
 
 
-Victories = {
-    GameAction.Rock: GameAction.Paper, 
-    GameAction.Paper: GameAction.Scissors, 
-    GameAction.Scissors: GameAction.Rock 
-}
 
 class RPS:
 
     def __init__(self):
-        self.victories = Victories
+
         self.game_result = GameResult
+        self.victories = {
+            GameAction.Rock: GameAction.Paper, 
+            GameAction.Paper: GameAction.Scissors, 
+            GameAction.Scissors: GameAction.Rock 
+        }
         
 
 
-    def assess_game(user_action, computer_action):
+    def assess_game(self, user_action, computer_action):
 
         game_result = None
 
@@ -38,36 +38,21 @@ class RPS:
             game_result = GameResult.Tie
 
         # You picked Rock
-        elif user_action == GameAction.Rock:
-            if computer_action == GameAction.Scissors:
-                print("Rock smashes scissors. You won!")
-                game_result = GameResult.Victory
-            else:
-                print("Paper covers rock. You lost!")
-                game_result = GameResult.Defeat
 
-        # You picked Paper
-        elif user_action == GameAction.Paper:
-            if computer_action == GameAction.Rock:
-                print("Paper covers rock. You won!")
-                game_result = GameResult.Victory
-            else:
-                print("Scissors cuts paper. You lost!")
-                game_result = GameResult.Defeat
+        elif computer_action == self.victories[user_action]:
+            print(f"Computer picked {computer_action.name} and user picked {user_action.name}. Computer wins!")
+            game_result = GameResult.Defeat
 
-        # You picked Scissors
-        elif user_action == GameAction.Scissors:
-            if computer_action == GameAction.Rock:
-                print("Rock smashes scissors. You lost!")
-                game_result = GameResult.Defeat
-            else:
-                print("Scissors cuts paper. You won!")
-                game_result = GameResult.Victory
+
+        else:
+            print(f"Computer picked {computer_action.name} and user picked {user_action.name}. User wins!")
+            game_result = GameResult.Victory
+    
 
         return game_result
 
 
-    def get_computer_action():
+    def get_computer_action(self):
         computer_selection = random.randint(0, len(GameAction) - 1)
         computer_action = GameAction(computer_selection)
         print(f"Computer picked {computer_action.name}.")
@@ -75,7 +60,7 @@ class RPS:
         return computer_action
 
 
-    def get_user_action():
+    def get_user_action(self):
         # Scalable to more options (beyond rock, paper and scissors...)
         game_choices = [f"{game_action.name}[{game_action.value}]" for game_action in GameAction]
         game_choices_str = ", ".join(game_choices)
@@ -85,27 +70,29 @@ class RPS:
         return user_action
 
 
-    def play_another_round():
+    def play_another_round(self):
         another_round = input("\nAnother round? (y/n): ")
         return another_round.lower() == 'y'
 
 
-    def main():
+    def main(self):
 
         while True:
             try:
-                user_action = get_user_action()
+                user_action = self.get_user_action()
             except ValueError:
                 range_str = f"[0, {len(GameAction) - 1}]"
                 print(f"Invalid selection. Pick a choice in range {range_str}!")
                 continue
 
-            computer_action = get_computer_action()
-            assess_game(user_action, computer_action)
+            computer_action = self.get_computer_action()
+            self.assess_game(user_action, computer_action)
 
-            if not play_another_round():
+            if not self.play_another_round():
                 break
 
 
 if __name__ == "__main__":
-    main()
+
+    game = RPS()
+    game.main()
